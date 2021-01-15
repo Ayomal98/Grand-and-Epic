e09f1e85290a9d4f3eb15e4cea389442dc6b5165
Ayomal98 <ayomalpraveen33@gmail.com>
2021-01-15 22:50:55 +0530

    modify the showtable-after section as same as the initial stage

--- src/Hotel_Website/showtables-after.php
+++ src/Hotel_Website/showtables-after.php
@@ -11,34 +11,63 @@ $selected = array(); //an empty array to store the selected tables(before)
 //loading tables for Lunch mealPeriod
 if ($mealPeriod == 'Lunch') {
     $sql1 = "SELECT Table_No,Date,Date_Format(Time,'%k:%i') as 'TimeDemo' from dinein_booking where Date='" . $dateSelected . "'";
+    $tables = "SELECT * FROM tables";
     $result1 = mysqli_query($con, $sql1);
+    $tableResult = mysqli_query($con, $tables);
     while ($row = mysqli_fetch_assoc($result1)) {
         if ($row["TimeDemo"] == $lunchPeriod) {
             array_push($selected, $row['Table_No']);
         }
     }
-    for ($i = 1; $i <= 15; $i++) {
-        if (in_array($i, $selected)) {
-            echo "<div class=\"dot not-avb \"><span>$i</span></div>";
+
+    while ($rowTable = mysqli_fetch_assoc($tableResult)) {
+        $table_no = $rowTable["Table_No"];
+        $no_Of_Customers = $rowTable["No_Customers"];
+        if (in_array($table_no, $selected)) {
+            echo "<div>
+                    <div class=\"dot not-avb \"><span>$table_no</span></div>
+                    <div style=\"margin-left:-40px\">Customers Allowed : $no_Of_Customers </div>
+                  </div>";
         } else {
-            echo "<div class=\"dot \"><span>$i</span></div>";
+            echo "<div>
+                    <div class=\"dot\"><span>$table_no</span></div>
+                    <div style=\"margin-left:-40px\">Customers Allowed : $no_Of_Customers </div>
+                  </div>";
         }
     }
+    // for ($i = 1; $i <= 15; $i++) {
+    //     if (in_array($i, $selected)) {
+    //         echo "<div class=\"dot not-avb \"><span>$i</span></div>";
+    //     } else {
+    //         echo "<div class=\"dot \"><span>$i</span></div>";
+    //     }
+    // }
 }
 //loading tables for Breakfast mealPeriod
 elseif ($mealPeriod == 'Breakfast') {
     $sql2 = "SELECT Table_No,Date,Date_Format(Time,'%k:%i') as 'TimeDemo' from dinein_booking where Date='" . $dateSelected . "'";
+    $tables = "SELECT * FROM tables";
     $result2 = mysqli_query($con, $sql2);
+    $tableResult = mysqli_query($con, $tables);
     while ($row = mysqli_fetch_assoc($result2)) {
         if ($row["TimeDemo"] == $breakfastPeriod) {
             array_push($selected, $row['Table_No']);
         }
     }
-    for ($i = 1; $i <= 15; $i++) {
-        if (in_array($i, $selected)) {
-            echo "<div class=\"dot not-avb \"><span>$i</span></div>";
+
+    while ($rowTable = mysqli_fetch_assoc($tableResult)) {
+        $table_no = $rowTable["Table_No"];
+        $no_Of_Customers = $rowTable["No_Customers"];
+        if (in_array($table_no, $selected)) {
+            echo "<div>
+                    <div class=\"dot not-avb \"><span>$table_no</span></div>
+                    <div style=\"margin-left:-40px\">Customers Allowed : $no_Of_Customers </div>
+                  </div>";
         } else {
-            echo "<div class=\"dot \"><span>$i</span></div>";
+            echo "<div>
+                    <div class=\"dot\"><span>$table_no</span></div>
+                    <div style=\"margin-left:-40px\">Customers Allowed : $no_Of_Customers </div>
+                  </div>";
         }
     }
 }
@@ -46,17 +75,27 @@ elseif ($mealPeriod == 'Breakfast') {
 //loading tables for Dinner mealPeriod
 else {
     $sql3 = "SELECT Table_No,Date,Date_Format(Time,'%k:%i') as 'TimeDemo' from dinein_booking where Date='" . $dateSelected . "'";
+    $tables = "SELECT * FROM tables";
     $result3 = mysqli_query($con, $sql3);
+    $tableResult = mysqli_query($con, $tables);
     while ($row = mysqli_fetch_assoc($result3)) {
         if ($row["TimeDemo"] == $dinnerPeriod) {
             array_push($selected, $row['Table_No']);
         }
     }
-    for ($i = 1; $i <= 15; $i++) {
-        if (in_array($i, $selected)) {
-            echo "<div class=\"dot not-avb \"><span>$i</span></div>";
+    while ($rowTable = mysqli_fetch_assoc($tableResult)) {
+        $table_no = $rowTable["Table_No"];
+        $no_Of_Customers = $rowTable["No_Customers"];
+        if (in_array($table_no, $selected)) {
+            echo "<div>
+                    <div class=\"dot not-avb \"><span>$table_no</span></div>
+                    <div style=\"margin-left:-40px\">Customers Allowed : $no_Of_Customers </div>
+                  </div>";
         } else {
-            echo "<div class=\"dot \"><span>$i</span></div>";
+            echo "<div>
+                    <div class=\"dot\"><span>$table_no</span></div>
+                    <div style=\"margin-left:-40px\">Customers Allowed : $no_Of_Customers </div>
+                  </div>";
         }
     }
 }
