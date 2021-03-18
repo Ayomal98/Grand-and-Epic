c5c384f7fc48dfba3839d473a2b160d86af04d59
Ayomal98 <ayomalpraveen33@gmail.com>
2021-03-18 12:02:24 +0530

    queries for retreive prices in payment page done

--- src/Hotel_Website/stayingin-user-payment.php
+++ src/Hotel_Website/stayingin-user-payment.php
@@ -31,11 +31,11 @@
                 <div class="user-details">
                     <label for="User-Deatils" style="font-weight: bolder;margin-top:-40px;font-size:45px;color:white;margin-left:50px;">User Details</label>
                     <div class="user-details">
-                        <input type="text" name="FName" id="" placeholder="First Name" style="padding:15px;width:300px;margin-left:-270px;border-radius:10px;border:none;">
-                        <input type="text" name="LName" id="" placeholder="Last Name" style="padding:15px;width:300px;margin-left:-270px;border-radius:10px;border:none;">
-                        <input type="number" name="ContactNo" id="" placeholder="Contact Number" style="padding:15px;width:300px;margin-left:-270px;border-radius:10px;border:none;">
-                        <input type="text" name="Address" id="" placeholder="Address" style="padding:15px;width:300px;margin-left:-270px;border-radius:10px;border:none;">
-                        <input type="text" name="City" id="" placeholder="City" style="padding:15px;width:300px;margin-left:-270px;border-radius:10px;border:none;">
+                        <input type="text" name="FName" id="" placeholder="First Name" style="padding:15px;width:300px;margin-left:-270px;border-radius:10px;border:none;" required>
+                        <input type="text" name="LName" id="" placeholder="Last Name" style="padding:15px;width:300px;margin-left:-270px;border-radius:10px;border:none;" required>
+                        <input type="number" name="ContactNo" id="" placeholder="Contact Number" style="padding:15px;width:300px;margin-left:-270px;border-radius:10px;border:none;" required>
+                        <input type="text" name="Address" id="" placeholder="Address" style="padding:15px;width:300px;margin-left:-270px;border-radius:10px;border:none;" required>
+                        <input type="text" name="City" id="" placeholder="City" style="padding:15px;width:300px;margin-left:-270px;border-radius:10px;border:none;" required>
                         <input type="hidden" name="staying_in_temp" value="<?php echo $_GET['temp_id'] ?>">
                     </div>
                 </div>
@@ -44,21 +44,23 @@
                 $stayingIn_ID = $_GET['temp_id'];
                 $select_Room_Data = mysqli_query($con, "SELECT * FROM stayingin_booking_temp WHERE StayingIn_ID='$stayingIn_ID'");
                 while ($row = mysqli_fetch_assoc($select_Room_Data)) {
+                    $total_amount = (int)$row['Room_Price'] + (int)$row['Meal_Price'];
+                    $advance_amount = $total_amount * 0.2;
                     echo '
                             <div class="payment-details">
                                 <div style="margin-top:-465px;font-weight:bolder;margin-left:450px"><label for="Payment-Deatils" style="font-size:45px;color:white;" ;>Payment Details</label></div>
                                 <fieldset style="width: 550px;height:450px;margin-top:55px;background-color:white;border-radius:10px;border:none;padding:15px;margin-left:405px">
                                     <div class="room-payment" style="font-weight: bolder;font-size:30px;margin-bottom:10px;"><u>Price For the Rooms</u></div>
-                                    <label for="">Suite Room x 2 </label><label for="" style="font-weight:bolder;position:absolute;top:31%;right:15%">&nbsp;Rs.50,000/=</label>
+                                    <label for="">' . $row['Room_Type'] . ' x ' . $row['No_Rooms'] . ' </label><label for="" style="font-weight:bolder;position:absolute;top:31%;right:15%">&nbsp;Rs.' . $row['Room_Price'] . '/=</label>
                                     <div class="room-payment" style="font-weight: bolder;font-size:30px;margin-bottom:10px;margin-top:10px;"><u>Price For Meals</u></div>
-                                    <label for="" style="font-weight:bolder;text-align:center;margin-left:50px;margin-bottom:20px;">Set Menu</label><br>
+                                    <label for="" style="font-weight:bolder;text-align:center;margin-left:50px;margin-bottom:20px;">' . $row['Meal_Selection'] . '</label><br>
                                     <!-- <label for="" style="margin-top:20px;margin-left:10px;">Day 1</label><label for="">&nbsp; Rs.3000 /=</label><br>
                                     <label for="" style="margin-top:20px;margin-left:10px;">Day 2</label><label for="">&nbsp; Rs.5000 /=</label><br> -->
-                                    <label for="total amount meals" style="font-weight: bolder;font-size:20px;">Total Amount for Meals </label>&nbsp;<span style="font-weight: bolder;position:absolute;top:49.5%;right:15%"> Rs.8000/=</span>
+                                    <label for="total amount meals" style="font-weight: bolder;font-size:20px;">Total Amount for Meals </label>&nbsp;<span style="font-weight: bolder;position:absolute;top:49.5%;right:15%"> Rs.' . $row['Meal_Price'] . '/=</span>
                                     <div class="total-amount" style="font-weight: bolder;font-size:30px;margin-bottom:10px;margin-top:20px;"><u>Total Amount</u></div>
-                                    <label for="">Total Amount for the Booking <br><br></label><span style="font-weight: bolder;position:absolute;top:60%;right:15%"> Rs.58,000/=</span>
+                                    <label for="">Total Amount for the Booking <br><br></label><span style="font-weight: bolder;position:absolute;top:60%;right:15%">' . $total_amount . '</span>
                                     <div class="total-amount" style="font-weight: bolder;font-size:30px;margin-bottom:10px;margin-top:-5px;"><u>Advance Amount</u></div>
-                                    <label for="" style="font-weight:bold">Total Amount * 20 % <br><br></label><span style="position: absolute;right:14%;top:70.5%;font-size:28px;font-weight:bolder">Rs.11,600/=</span>
+                                    <label for="" style="font-weight:bold">Total Amount * 20 % <br><br></label><span style="position: absolute;right:14%;top:70.5%;font-size:28px;font-weight:bolder">Rs.' . $advance_amount . '/=</span>
                                 </fieldset>
             
                             </div>
